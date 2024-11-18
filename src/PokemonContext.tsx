dd9fab89db5a1259f0cad017d947d48c7bbf7ca0
Justine Manalili Pham <tinemanalili@gmail.com>
2024-11-18 12:57:11 +0100

    Updates

--- src/PokemonContext.tsx
+++
@@ -1,8 +0,0 @@
-import { createContext, useContext } from 'react';
-import { PokemonClient } from 'pokenode-ts';
-
-// API context
-const pokemonApiClient = new PokemonClient();
-export const PokemonContext = createContext(pokemonApiClient);
-
-export const useApi = () => useContext(PokemonContext);
